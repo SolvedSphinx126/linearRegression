da188db11db18f361028bf78ccb5c87068bd711c
Ben O'Bryan <obryanbw@gmail.com>
2023-09-13 21:07:52 -0500

    merge

--- linearRegression.py
+++ linearRegression.py
@@ -24,26 +24,29 @@ def leastSquares():
     offset = (sumY - slope * sumX)/m
     print(f"slope = {slope}, offset = {offset}")
 
-def cost(t0, t1):
+def J(t0, t1):
     cost = 0.0
     for i in range(0, m):
-        cost += ((t1*x[i]+t0)-(y[i]))**2
+        cost += (h(x[i])-(y[i]))**2
     return (1/(2*m))*cost
+
+def h(x):
+    return theta0 + theta1 * x
 
 def d0():
     sum = 0
     for i in range(0,m):
-        sum += theta0 + theta1 * x[i] - y[i]
+        sum += h(x[i]) - y[i]
     return (1/m) * sum
 
 def d1():
     sum = 0
     for i in range(0,m):
-        sum += (theta0 + theta1 * x[i] - y[i]) * x[i]
+        sum += (h(x[i]) - y[i]) * x[i]
     return (1/m) * sum
 
 def change():
-    return math.sqrt((-alpha * d0())**2+(-alpha*d1())**2)
+    return math.sqrt((-alpha * d0())**2 + (-alpha * d1())**2)
 
 while (change() >= epsilon):
     delta0 = d0()
@@ -51,9 +54,20 @@ while (change() >= epsilon):
     theta0 = theta0 - alpha * delta0
     theta1 = theta1 - alpha * delta1
 
-print(f"The final error is {cost(theta0, theta1)}")
+    print(f"Theta 0 is {theta0}")
+    print(f"Theta 1 is {theta1}")
+
+delta0 = d0()
+delta1 = d1()
+theta0 = theta0 - alpha * delta0
+theta1 = theta1 - alpha * delta1
+
+print(f"Theta 0 is {theta0}")
+print(f"Theta 1 is {theta1}")
+
+print(f"The final error is {J(theta0, theta1)}")
 leastSquares()
 
-plt.scatter(x,y)
-plt.plot(x, [theta0 + xi * theta1 for xi in x], color='red')
+plt.scatter(x, y, color='red', marker='x', alpha=0.5)
+plt.plot(x, [theta0 + xi * theta1 for xi in x], color='blue', linewidth=0.5, alpha=0.5)
 plt.show()
