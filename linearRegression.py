Fix normalization of data

linearRegression.py
@@ -6,19 +6,15 @@ import numpy as np
 
 # Initialize empty lists and variables
 m = 0  # Number of data points
-epsilon = 0.000001  # Convergence threshold for gradient descent
+epsilon = 0.001  # Convergence threshold for gradient descent
 alpha = 0.01  # Learning rate for gradient descent
 theta = np.array([])  # column vector of hypothesis
 
-# Read data from the "data.txt" file and populate data list with normalized data
+# Read data from the "data.txt" file and populate ndata frame with normalized data
 data = pd.read_csv("data.csv")
-means = []
-stddevs = []
-for col in data:
-    for row in data.index:
-        data[col][row] = (data[col][row] - np.mean(data[col])) / np.std(data[col])
+nData = data.apply(lambda col : [(x - np.mean(col)) / np.std(col) for x in col], raw=True)
 
-data.to_csv("normalized_data.csv")
+nData.to_csv("normalized_data.csv")
 
 
 # x = data[["NOX"]]
@@ -55,8 +51,8 @@ def calcConvergence(oldThetas, newThetas):
 
 
 prevTheta = np.zeros((2, 1))
-x = np.array(data[["DIS", "RAD"]])
-y = np.array(data[["NOX"]])
+x = np.array(nData[["DIS", "RAD"]])
+y = np.array(nData[["NOX"]])
 
 # Calculate the first delta theta before entering the loop
 theta = np.zeros((2, 1))